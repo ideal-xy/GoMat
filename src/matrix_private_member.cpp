e1f91cf586509b657652d5846dfe387def880aed
xuyan <yxu422325@gmail.com>
2025-09-03 22:02:00 +0800

    完善了一下

--- src/matrix_private_member.cpp
+++ src/matrix_private_member.cpp
@@ -3,10 +3,10 @@
 using namespace gomat;
 double& Matrix::getElement(size_t row,size_t col)
 {
-    return m_is_contiguous ? m_data[row * m_cols + col] : m_mat[row][col];
+    return m_is_contiguous ? m_data[col * m_rows + row] : m_mat[row][col];
 }
 
 double Matrix::getEle(size_t row,size_t col) const
 {
-    return m_is_contiguous ? m_data[row * m_cols + col] : m_mat[row][col];
+    return m_is_contiguous ? m_data[col * m_rows + row] : m_mat[row][col];
 }
