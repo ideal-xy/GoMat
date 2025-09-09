4401af2b940bc0c48953ab118f4cef594b8a6d94
xuyan <yxu422325@gmail.com>
2025-09-09 21:47:43 +0800

    简单修改

--- src/matrix_private_member.cpp
+++ src/matrix_private_member.cpp
@@ -1,12 +1,12 @@
 #include "matrix.h"
 
 using namespace gomat;
-double& Matrix::getElement(size_t row,size_t col)
+inline double& Matrix::getElement(size_t row,size_t col)
 {
     return m_is_contiguous ? m_data[col * m_rows + row] : m_mat[row][col];
 }
 
-double Matrix::getEle(size_t row,size_t col) const
+inline double Matrix::getEle(size_t row,size_t col) const
 {
     return m_is_contiguous ? m_data[col * m_rows + row] : m_mat[row][col];
 }
