5476eeda47e4a4307dd240d82c91d8aad52ea73c
来自星星的ideal <yxu422325@gmail.com>
2025-09-10 14:42:54 +0800

    更改编译器，以支持eigen使用openmp

--- auto_run.sh
+++ auto_run.sh
@@ -1,21 +1,23 @@
-#!/bin/bash
-# 正确的脚本
+
 set -e
 
-BUILD_DIR="build_test" # 建议改名以匹配真实架构
+BUILD_DIR="build_test" 
 
-echo "--- 正在清理旧的构建目录... ---"
+echo "清理旧的构建目录.."
 rm -rf ${BUILD_DIR}
 
-echo "--- 正在为本地 arm64 架构配置 CMake... ---"
+echo "配置 CMake... "
 
-cmake -B ${BUILD_DIR} -DCMAKE_BUILD_TYPE=Debug
+cmake -B ${BUILD_DIR} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_COMPILER=/opt/homebrew/opt/llvm/bin/clang++ \
+    -DCMAKE_C_COMPILER=/opt/homebrew/opt/llvm/bin/clang
 
-echo "--- 正在构建项目... ---"
+echo "构建项目... "
 cmake --build ${BUILD_DIR} 
 
-# 假设测试脚本在项目根目录的 test 文件夹下
-echo "--- 正在运行测试... ---"
+
+echo "运行测试... "
 cd ${BUILD_DIR}
 
 cd ../test
