cc25a5539157e66aaca5a2ca6c01b6292378f659
来自星星的ideal <yxu422325@gmail.com>
2025-09-13 15:19:19 +0800

    更新了自动化执行脚本

--- auto_run.sh
+++ auto_run.sh
@@ -1,24 +1,22 @@
+# 这个自动化脚本是用来测试的，而非debug，debug的目录位于gomat/debug
 
 set -e
 
-BUILD_DIR="build_test" 
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+echo "脚本所在的根目录是: ${SCRIPT_DIR}"
 
-echo "清理旧的构建目录.."
+BUILD_DIR="${SCRIPT_DIR}/build_test"  
+
+echo "--清理旧的构建目录--"
 rm -rf ${BUILD_DIR}
 
-echo "配置 CMake... "
-
+echo "--配置 CMake--(Release Type)"
 cmake -B ${BUILD_DIR} \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_CXX_COMPILER=/opt/homebrew/opt/llvm/bin/clang++ \
-    -DCMAKE_C_COMPILER=/opt/homebrew/opt/llvm/bin/clang
+    -DCMAKE_CXX_COMPILER=/opt/homebrew/opt/llvm/bin/clang++ 
 
-echo "构建项目... "
-cmake --build ${BUILD_DIR} 
+echo "--构建项目--" 
+cmake --build ${BUILD_DIR}
 
-
-echo "运行测试... "
-cd ${BUILD_DIR}
-
-cd ../test
-python test.py
+echo "--运行测试--"
+"${BUILD_DIR}/test/tes"
